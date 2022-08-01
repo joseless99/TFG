212d0aa0bba53f5e8a1fb0454ea38cd459b36d77
joseless99 <uco_jj@UCO.SYSTEM>
2022-08-01 10:13:08 +0200

    Añadida funcion uSensor() que controla el sensor emisor-receptor ultrasonico del vehiculo arduino
    Cambiados los pines de los motores: de Pines Digitales a Pines Power Modulation

--- sketch_TFG/sketch_TFG.ino
+++ sketch_TFG/sketch_TFG.ino
@@ -1,17 +1,18 @@
 /**
  * Este es el sketch a cargar el en arduino del coche.
  * Este tendra conectados 2 motores de 5V DC ademas de un modulo bluetooth, por el cual se 
- * reciben instrucciones de acciones a realizar. El emisor de las instrucciones es una aplicacion
- * Android desarrollada para este proyecto.
+ * reciben instrucciones de acciones a realizar, y un modulo emisor-receptor de ondas sonicas para calcular la distancia con el objeto enfrente del mismo. 
+ * El emisor de las instrucciones es una aplicacion Android desarrollada para este proyecto.
  * 
  * @author: Juan Jose Ropero Cerro
  * 
  */
-#define M1AVANCE 8
-#define M1RETROCESO 9
+#define M1AVANCE 6
+#define M1RETROCESO 5 
 #define M2AVANCE 10
 #define M2RETROCESO 11
-
+#define TRIGGER 8
+#define ECHO 7
 char dato;//Variable que almacena los datos que se leen del modulo bluetooth
 void setup() {
  //Iniciamos monitores a usar.Serial es solo usado para verificar el envio y recepcion de datos correcto
@@ -23,19 +24,23 @@ void setup() {
   pinMode(M1RETROCESO,OUTPUT);
   pinMode(M2AVANCE,OUTPUT);
   pinMode(M2RETROCESO,OUTPUT);
-
+  pinMode(TRIGGER,OUTPUT);
+  pinMode(ECHO,INPUT);
+  
   digitalWrite(M1AVANCE,LOW);
   digitalWrite(M1RETROCESO,LOW);
   digitalWrite(M2AVANCE,LOW);
   digitalWrite(M2RETROCESO,LOW);
+  digitalWrite(TRIGGER,LOW);
 
   dato=' ';
   
 }
+
 void loop() {
-  
-  //Verificamos si existen datos recibidos por el canal de comunicacion en el que esta el modulo bluetooth
-  if(Serial1.available()>0){
+  if(uSensor()<=5.0){
+    
+  }else if(Serial1.available()>0){//Verificamos si existen datos recibidos por el canal de comunicacion en el que esta el modulo bluetooth
       
       dato=Serial1.read();//Guardamos los datos del canal de comunicacion BT en la variable dato
       
@@ -106,8 +111,8 @@ void avance(){
  */
 void retroceso(){
 
-    digitalWrite(M1RETROCESO,HIGH);
-    digitalWrite(M2RETROCESO,HIGH);
+    analogWrite(M1RETROCESO,127);
+    analogWrite(M2RETROCESO,127);
 }
 
 /**
@@ -128,4 +133,26 @@ void giroDerecha(){
 
     digitalWrite(M1AVANCE,HIGH);
     digitalWrite(M2RETROCESO,HIGH);
+}
+
+/**
+ * Funcion que usa el sensor ultrasonico para calcular la distancia del vehiculo con un posible obstaculo que haya frente al mismo.
+ * 
+ * Esta activa el pin del emisor del sensor ultrasonico(Trigger) por un pequeño periodo de tiempo. Al activarlo a HIGH, el pin receptor (Echo) 
+ * tambien cambia a estado HIGH, indicando que esta recibiendo la onda sonica del sensor(cambiara a LOW una vez deje de recibir señales). 
+ * Tras cambiar el pin Trigger a LOW, mediremos el tiempo que el pin Echo estuvo en HIGH en microsegundos y junto con la velocidad de la onda sonica (340 m/s)
+ * y el delay que el pin Trigger estuvo activo calculamos la distancia del vehiculo al objeto mas proximo
+ */
+float uSensor(){
+
+  //Activamos el pin emisor de la onda sonica(Trigger)
+  digitalWrite(TRIGGER,HIGH);
+  delayMicroseconds(3);//delay de funcionamiento del pin emisor
+  //Desactivamos el pin emisor
+  digitalWrite(TRIGGER,LOW);
+  long timer=pulseIn(ECHO,HIGH);//devuelve el tiempo que ha estado en HIGH en microsegundos
+  //ecuacion de calculo de la distancia
+  //0.034= velocidad de la onda (340m/s)
+  //3= tiempo establecido del delay del pin trigger 
+  return(timer*0.034/3);
 }
