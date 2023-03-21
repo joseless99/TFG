4e92e796e8f4fcf74a6ea69b5cd224f458baf443
Juan Jose Ropero Cerro <i82rocej@uco.es>
2023-03-21 12:02:16 +0100

    Actualizada documentacion de sketch y mejorado calculo de distancia del HC-SR04

--- sketch_TFG_L293D_version/sketch_TFG_L293D_version.ino
+++ sketch_TFG_L293D_version/sketch_TFG_L293D_version.ino
@@ -22,23 +22,24 @@
 #define TRIGGER 8
 #define ECHO 7
 char dato;//Variable que almacena los datos que se leen del modulo bluetooth
-bool activo;
+bool activo;//Estado de conexion con el sistema Maestro
 void setup() {
  //Iniciamos monitores a usar.Serial es solo usado para verificar el envio y recepcion de datos correcto
-  Serial.begin(9600);//Deshabilitar para la version final
+  Serial.begin(9600);//Borrar para la version final
   Serial1.begin(9600);//Monitor de comunicacion para el modulo bluetooth.Conectar dicho modulo a pines 0,1
   
   //Configracion inical de los pines de los motores y de variables globales usadas
   pinMode(M1ENABLE,OUTPUT);
   pinMode(M1AVANCE,OUTPUT);
   pinMode(M1RETROCESO,OUTPUT);
+  
   pinMode(M2ENABLE,OUTPUT);
   pinMode(M2AVANCE,OUTPUT);
   pinMode(M2RETROCESO,OUTPUT);
   pinMode(TRIGGER,OUTPUT);
   pinMode(ECHO,INPUT);
   
-  //Establecemos todo pin de salida a LOW
+  //Establecemos todo pin de salida a los motores a LOW
   digitalWrite(M1ENABLE,LOW);
   digitalWrite(M1AVANCE,LOW);
   digitalWrite(M1RETROCESO,LOW);
@@ -52,6 +53,7 @@ void setup() {
 }
 
 void loop() {
+  Serial.println(activo);//Borrar para version final
   if(activo==true){
         if(Serial1.available()>0){//Verificamos si existen datos recibidos por el canal de comunicacion en el que esta el modulo bluetooth
           
@@ -92,12 +94,13 @@ void loop() {
             activo=false;//Impedimos que el sistema siga funcionando
             
             //Desactivamos los motores
-            digitalWrite(M1ENABLE,HIGH);
-            digitalWrite(M2ENABLE,HIGH);
+            digitalWrite(M1ENABLE,LOW);
+            digitalWrite(M2ENABLE,LOW);
             
             Serial.print("Desactivado\n");//Mensaje de prueba en el arduino. Borrar en la version final
           }       
       }
+      Serial.println(dato);//Borrar
       //Enviamos al dispositivo maestro la distancia del vehiculo con un obstaculo frontal
       Serial1.println(uSensor());
       
@@ -117,9 +120,16 @@ void loop() {
         }else{
      
           activo=false;
+          
+          //Desactivamos los motores del coche
+          digitalWrite(M1ENABLE,LOW);
+          digitalWrite(M2ENABLE,LOW);
         }
      }
+     
   }
+  //Delay para pruebas y depuracion.Borrar o reducir
+  delay(100);
 }
 
 /**
@@ -133,7 +143,7 @@ void frenado(){
   digitalWrite(M1RETROCESO,LOW);
   digitalWrite(M2AVANCE,LOW);
   digitalWrite(M2RETROCESO,LOW);
-  delay(50);
+  delay(10);
 }
 
 /**
@@ -153,8 +163,8 @@ void avance(){
  */
 void retroceso(){
 
-    analogWrite(M1RETROCESO,127);
-    analogWrite(M2RETROCESO,127);
+    digitalWrite(M1RETROCESO,HIGH);
+    digitalWrite(M2RETROCESO,HIGH);
 }
 
 /**
@@ -185,16 +195,20 @@ void giroDerecha(){
  * Tras cambiar el pin Trigger a LOW, mediremos el tiempo que el pin Echo estuvo en HIGH en microsegundos y junto con la velocidad de la onda sonica (340 m/s)
  * y el delay que el pin Trigger estuvo activo calculamos la distancia del vehiculo al objeto mas proximo
  */
+
 float uSensor(){
 
   //Activamos el pin emisor de la onda sonica(Trigger)
   digitalWrite(TRIGGER,HIGH);
-  delayMicroseconds(3);//delay de funcionamiento del pin emisor
+  delayMicroseconds(10);//delay de funcionamiento del pin emisor
   //Desactivamos el pin emisor
   digitalWrite(TRIGGER,LOW);
-  long timer=pulseIn(ECHO,HIGH);//devuelve el tiempo que ha estado en HIGH en microsegundos
-  //ecuacion de calculo de la distancia
-  //0.034= velocidad de la onda (340m/s)
-  //3= tiempo establecido del delay del pin trigger 
-  return(timer*0.034/3);
-}
+  long timer=pulseIn(ECHO,HIGH);//devuelve el tiempo que el pin hecho ha tardado de pasar de LOW a HIGH en microsegundos
+ 
+  /**ecuacion de calculo de la distancia
+   * timer: Tiempo que se ha tardado en recibir la onda emitida por el pin TRIGGER del HC-SR04
+   * 0.01715: Se obtiene de dividir la velocidad de la onda ultrasonica (343m/s =>0.0343cm/microsegundos) entre 2, 
+   * ya que la onda recorre 2 veces la distancia del vehiculo al coche (ida y vuelta)
+   */ 
+  return(timer*0.01715);
+ }
